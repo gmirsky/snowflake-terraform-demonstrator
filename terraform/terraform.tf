2afa063e936488d0429c3698b8ab08abd8011496
Gregory Mirsky <6909025+gmirsky@users.noreply.github.com>
2023-03-03 15:18:56 -0500

    updated provider

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     snowflake = {
       source  = "Snowflake-Labs/snowflake"
-      version = "0.56.5"
+      version = "0.57.0"
     }
     tls = {
       source  = "hashicorp/tls"
